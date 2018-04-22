62ebc75facfaa34cccdbd83edd26dcbfd823a05d
Haotian Liu <liuhaotian.cn@gmail.com>
2018-04-22 22:03:17 +0800

    Implement basic color mapper.

--- src/ColorMapper/ColorMapper.cpp
+++ src/ColorMapper/ColorMapper.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "ColorMapper.h"
+#include <fstream>
+
+void ColorMapper::update_config() {
+    load_keyframes();
+    base_map();
+    exit(-1);
+}
+
+void ColorMapper::load_keyframes() {
+    keys.clear();
+    images_grey.clear();
+    images_color.clear();
+
+    const std::string path_folder = "/Users/haotian/Downloads/fountain_all/";
+    const std::string img_folder = path_folder + "vga/";
+    const std::string path_file = "key_convert.txt";
+    std::fstream keyFrameFile(path_folder + path_file);
+
+    std::string imagePath;
+    while (!keyFrameFile.eof()) {
+        keyFrameFile >> imagePath;
+        keys.push_back(img_folder + imagePath);
+    }
+
+    cv::Mat color;
+    cv::Mat grey;
+
+    for (auto &key : keys) {
+        color = cv::imread(key);
+        cv::cvtColor(color, grey, CV_RGB2GRAY);
+
+        images_color.push_back(color);
+        images_grey.push_back(grey);
+    }
+}
+
+void ColorMapper::base_map() {
+
+}
--- src/ColorMapper/ColorMapper.h
+++ src/ColorMapper/ColorMapper.h
@@ -5,6 +5,8 @@
 #ifndef INC_3DRECONSTRUCTION_COLORMAPPER_H
 #define INC_3DRECONSTRUCTION_COLORMAPPER_H
 
+#include <vector>
+#include "opencv/cv.hpp"
 #include "Geometry/Shape.h"
 
 class ColorMapper {
@@ -12,9 +14,16 @@ public:
     ColorMapper() = default;
     explicit ColorMapper(Shape *shape) : shape(shape) {}
     void switch_to(Shape *shape) { this->shape = shape; }
+    void update_config();
     bool available() const { return shape != nullptr; }
 private:
+    void load_keyframes();
+    void base_map();
     Shape *shape = nullptr;
+
+    std::vector<std::string> keys;
+    std::vector<cv::Mat> images_color;
+    std::vector<cv::Mat> images_grey;
 };
 
 
