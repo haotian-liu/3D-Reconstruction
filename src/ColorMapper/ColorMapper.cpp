5e8ac8165b26b614ab091d3bafcc96b4904bac06
Haotian Liu <liuhaotian.cn@gmail.com>
2018-04-26 10:40:28 +0800

    Use average color mapping.

--- src/ColorMapper/ColorMapper.cpp
+++ src/ColorMapper/ColorMapper.cpp
@@ -14,7 +14,7 @@ void ColorMapper::map_color() {
 void ColorMapper::load_keyframes() {
     map_units.clear();
 
-    const std::string path_folder = "output/1524498778/";
+    const std::string path_folder = "output/1524709889/";
     const std::string path_file = "pose.log";
     std::fstream keyFrameFile(path_folder + path_file);
 
@@ -113,6 +113,8 @@ void ColorMapper::base_map() {
     glEnable(GL_DEPTH_TEST);
     glDepthMask(GL_TRUE);
     glDepthFunc(GL_LEQUAL);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
 
     glViewport(0, 0, frameWidth, frameHeight);
 
@@ -121,6 +123,8 @@ void ColorMapper::base_map() {
 
     auto mapped_count = new int[shape->vertices.size()];
     memset(mapped_count, 0, sizeof(int) * shape->vertices.size());
+
+    std::fill(shape->colors.begin(), shape->colors.end(), glm::vec4(0.f));
 
     for (const auto &mapper : map_units) {
         glm::mat4 transform = projMatrix * mapper.transform;
@@ -149,6 +153,18 @@ void ColorMapper::base_map() {
         glReadPixels(0, 0, frameWidth * msaa, frameHeight * msaa, GL_DEPTH_COMPONENT, GL_FLOAT, screenshot_raw);
         screenshot_data = cv::Mat(frameHeight * msaa, frameWidth * msaa, CV_32F, screenshot_raw);
 
+        cv::Mat gray = screenshot_data, grad, grad_x, grad_y, kernel;
+
+        kernel = (cv::Mat_<int>(3, 3) << 0, -1, 0, 0, 2, 0, 0, -1, 0);
+        cv::filter2D(gray, grad_x, -1, kernel);
+        grad_x = cv::abs(grad_x);
+
+        kernel = (cv::Mat_<int>(3, 3) << 0, 0, 0, -1, 2, -1, 0, 0, 0);
+        cv::filter2D(gray, grad_y, -1, kernel);
+        grad_y = cv::abs(grad_y);
+
+        cv::addWeighted(grad_x, 0.5, grad_y, 0.5, 0, grad);
+
         glBindFramebuffer(GL_FRAMEBUFFER, fboMsaa);
 
 //        cv::imshow("win", screenshot_data);
@@ -161,24 +177,33 @@ void ColorMapper::base_map() {
             vert /= vert.w;
             cx = (vert.x + 1) * frameWidth / 2;
             cy = (vert.y + 1) * frameHeight / 2;
+
+            if (cx < 3 || cx + 3 > frameWidth || cy < 3 || cy + 3 > frameHeight) {
+                continue;
+            }
+
             float pixel = screenshot_data.at<float>(cy, cx);
+//            float gradient = grad.at<float>(cy, cx);
+//            if (gradient > 0.0000001) continue;
 //            printf("%f %f\n", pixel, z);
             if (std::fabs(pixel - z) < 0.00002f) {
                 cx = (vert.x + 1) * 320;
                 cy = (vert.y + 1) * 240;
                 cv::Vec3b pixel = mapper.color_image.at<cv::Vec3b>(cy, cx);
-//                shape->colors[i] *= mapped_count[i];
-                shape->colors[i] = glm::vec4(
+                shape->colors[i] *= mapped_count[i];
+                shape->colors[i] += glm::vec4(
 //                        1.f, 0.f, 0.f,
                         pixel.val[2] / 255.f,
                         pixel.val[1] / 255.f,
                         pixel.val[0] / 255.f,
                         1.f
                 );
-//                shape->colors[i] /= ++mapped_count[i];
+                shape->colors[i] /= ++mapped_count[i];
             }
         }
 //        break;
+        static int count = 0;
+//        if (++count >= 5) break;
     }
 
     delete[]mapped_count;
@@ -188,4 +213,6 @@ void ColorMapper::base_map() {
     glDeleteRenderbuffers(1, &rboMsaa);
     glDeleteFramebuffers(1, &fboMsaa);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    glDisable(GL_CULL_FACE);
 }
